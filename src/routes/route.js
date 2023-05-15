7dc592efc674d0417661c78a1b23916027a6cdc6
pravin <pravin.patekar@simbaquartz.com>
2023-05-15 17:13:46 +0530

    changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const customerController = require("../controller/customerController");
+const productController = require("../controller/productController");
 const middleware = require("../middleware/authentication");
 //API
 router.get(
@@ -8,11 +9,13 @@ router.get(
   middleware.demoMiddleware,
   customerController.allCustomer
 );
-
+// customer routes
 router.get("/customer/:id", customerController.singleCustomer);
 router.put("/customer/:id", customerController.updateCustomer);
 router.delete("/customer/:id", customerController.deleteCustomer);
 router.post("/login", customerController.loginCustomer);
 router.post("/customer", customerController.createCustomer);
 
+//oder routes
+router.post("/order", productController.createOrder);
 module.exports = router;
